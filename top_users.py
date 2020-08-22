697f0d42fe229d640490444cda4fa786585d6517
Egor <veli4estva@gmail.com>
2020-08-22 20:47:15 +0400

    added writting tasks in file

--- top_users.py
+++ top_users.py
@@ -3,6 +3,7 @@ import json
 with open('data_file.json', 'r') as read_file:
     todos = json.load(read_file)
 
+#  Searching for top users
 todos_by_user = {}
 for todo in todos:
     if todo['completed']:
@@ -21,3 +22,15 @@ for user, tasks_completed in todos_by_user:
     top_users.append(str(user))
 
 print('Users ' + ', '.join(top_users) + f' has completed max tasks ({max_complete})')
+
+
+#  Writing completed tasks of top users in filtered_data_file.json
+def keep(todo):
+    is_complete = todo['completed']
+    is_top_user = str(todo['userId']) in top_users
+    return is_complete and is_top_user
+
+
+with open('filtered_data_file.json', 'w') as write_file:
+    filtered_todos = list(filter(keep, todos))
+    json.dump(filtered_todos, write_file, indent=3)
